6ea8cd41040c7887f00031b1baaaa58eaab12528
16022970 <16022970@myrp.edu.sg>
2018-04-24 10:54:37 +0800

    Set images 3 - 5

--- app/src/main/java/com/example/a16022970/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/a16022970/demoimageview/MainActivity.java
@@ -8,6 +8,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Declare the ImageView object called ivDay2
     ImageView ivDay2;
+    ImageView ivDay3;
+    ImageView ivDay4;
+    ImageView ivDay5;
+
 
 
     @Override
@@ -16,8 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Get the ImageView obj and assign to ivDay2
         ivDay2 = (ImageView)findViewById(R.id.imageView2);
+        ivDay3 = (ImageView)findViewById(R.id.imageView3);
+        ivDay4 = (ImageView)findViewById(R.id.imageView4);
+        ivDay5 = (ImageView)findViewById(R.id.imageView5);
+
         //Set the Image
         ivDay2.setImageResource(R.drawable.day2);
+        ivDay3.setImageResource(R.drawable.day3);
+        ivDay4.setImageResource(R.drawable.day4);
+        ivDay5.setImageResource(R.drawable.day5);
+
 
 
 
